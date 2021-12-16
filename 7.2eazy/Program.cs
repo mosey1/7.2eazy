cec5e6c9a318704e56dd1593d0b0f02e6e974f2a
Dababy <Dababy@DESKTOP-79IU4EK>
2021-12-16 11:29:38 +0200

    Добавьте файлы проекта.

---
+++ 7.2eazy/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _7._2eazy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите текст:");
+            string word = Console.ReadLine();
+            Console.WriteLine("Оригинальный текст: " + word);
+            word = word.Replace(" ", "");
+            Console.WriteLine("Передвинутый текст: " + word);
+            Console.ReadLine();
+        }
+    }
+}
